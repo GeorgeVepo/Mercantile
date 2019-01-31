205b11b12c7b278685b64a06a003b90626ea74e4
richardvepogg <richardvepogg@gmail.com>
2019-01-30 22:33:14 -0200

    Update principal.js
    path do workspace

--- Puppeteer/Principal/principal.js
+++ Puppeteer/Principal/principal.js
@@ -7,19 +7,22 @@ module.exports = {
         const page = await browser.newPage();
         await page.setViewport({width: 1050, height: 1040});
        
+        /*
         //página de logon
         await page.goto('https://www.mercadolivre.com/jms/mlb/lgz/login?go=https%3A%2F%2Fwww.mercadolivre.com.br%2F&loginType=explicit#menu-user');
 
+        page.waitFor(500);
         //login
         page.type(" #login_user_form > div.auth-form__row > div > div > label > div.andes-form-control__control > span", 'testepuppeteer@gmail.com');
        
+        page.waitFor(500);
         //botão prosseguir
         page.click('<input type="submit" class="ui-button ui-button--primary auth-button auth-button--user" value="Continuar">');
 
         page.waitFor(500);
         //senha
         //page.type('<input type="submit" class="ui-button ui-button--primary auth-button auth-button--user" value="Continuar">', text[, options])
-
+*/
 
         await page.goto('https://lista.mercadolivre.com.br/s9-galaxy-plus-128gb_OrderId_PRICE_ItemTypeID_N_PriceRange_2500-0_BestSellers_YES');          
         await page.waitForSelector('.item__price')
@@ -50,5 +53,6 @@ module.exports = {
             url = await page.evaluate(element => element.href, listUrl[i]);                   
             listOffers[i] = title + "###" + fraction + "," + decimal + "###" + url;
         } 
+        
     }
 }
